e3c9ba4602bd8607b55ceeb571b103752c99eee0
Hugh Harris <dhharris9@gmail.com>
2018-08-02 20:19:33 -0700

    first commit

---
+++ update_all.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+BREW="/usr/local/bin/brew"
+PIP_REVIEW="/usr/local/bin/pip-review"
+PIP="/usr/local/bin/pip"
+RUST_BIN="$HOME/.cargo/bin"
+
+# Add a timestamp to the logfile
+echo "------------------------[" $(/bin/date) "]------------------------"
+
+# Update Homebrew package list and upgrade packages that are out of date
+echo "Updating Homebrew..."
+$BREW update
+$BREW upgrade
+
+# Update Vim plugins
+echo "Updating vim plugins..."
+sh ~/Scripts/update_vim_plugins.sh
+
+# Update pip
+echo "Updating pip..."
+$PIP install --upgrade pip
+
+# Update pip packages
+echo "Updating pip packages..."
+$PIP_REVIEW --auto
+
+# Update rustc and cargo packages
+echo "Updating rust toolchain..."
+$RUST_BIN/rustup -v update nightly
+
+echo "Updating cargo packages..."
+$RUST_BIN/cargo install-update --all
+
+# Cleanup
+echo "Cleaning up..."
+$BREW cleanup
+
+echo "Done!"
