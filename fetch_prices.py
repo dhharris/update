94b165421a9e85455349ed2b900735140fcc7325
Hugh Harris <dhharris9@gmail.com>
2019-01-10 19:09:26 -0800

    migrate to python 3

--- fetch_prices.py
+++ fetch_prices.py
@@ -1,36 +1,37 @@
-import urllib
 import json
 import os
 
+from urllib.request import urlopen
+
 # The notifier function
 def notify(title, subtitle, message):
-    t = '-title {!r}'.format(title)
-    s = '-subtitle {!r}'.format(subtitle)
-    m = '-message {!r}'.format(message)
-    os.system('/usr/local/bin/terminal-notifier {}'.format(' '.join([m, t, s])))
+    t = "-title {!r}".format(title)
+    s = "-subtitle {!r}".format(subtitle)
+    m = "-message {!r}".format(message)
+    os.system("/usr/local/bin/terminal-notifier {}".format(" ".join([m, t, s])))
 
 def print_item_price(id):
-    '''
+    """
     Given an item id, prints data about the item
     and its price on the Grand Exchange
-    '''
-    base_url = 'http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item='
+    """
+    base_url = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item="
     url = base_url + str(id)
     # Get json object from server
-    response = urllib.urlopen(url)
+    response = urlopen(url)
     data = json.loads(response.read())
 
-    # Print header
-    print data['item']['name']
-    print data['item']['description']
+    # print(header
+    print(data["item"]["name"])
+    print(data["item"]["description"])
 
-    #Print the current price
-    print 'Current price: ' + str(data['item']['current']['price'])
+    # print the current price
+    print("Current price: " + str(data["item"]["current"]["price"]))
 
-    # Print the price change for today
-    print 'Change: ' + str(data['item']['today']['price'])
+    # print(the price change for today
+    print("Change: " + str(data["item"]["today"]["price"]))
 
-    print # Newline so multiple entries have a gap
+    print()
 
 # astral rune
 print_item_price(9075)
@@ -38,5 +39,5 @@ print_item_price(9075)
 # cannonball
 #print_item_price(2)
 
-notify('Grand Exchange', 'Prices updated', 'See /tmp/runescape.log for info')
-os.system('/usr/bin/open /tmp/runescape.log')
+notify("Grand Exchange", "Prices updated", "See /tmp/runescape.log for info")
+os.system("/usr/bin/open /tmp/runescape.log")
