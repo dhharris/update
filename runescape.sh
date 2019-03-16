5d5649bb32d0ac5dd14be8b70f9d26704cc83248
Hugh Harris <dhharris9@gmail.com>
2019-03-16 09:13:56 -0700

    More elegant shell script

--- runescape.sh
+++ runescape.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
+python3 "$HOME"/update/fetch_prices.py > /tmp/runescape.log
+
 # Terminal notification
-/usr/bin/python /Users/hugh/Scripts/fetch_prices.py | wall
-
-# Write to /tmp/runescape.log
-/usr/bin/python /Users/hugh/Scripts/fetch_prices.py > /tmp/runescape.log
-
+wall < /tmp/runescape.log
