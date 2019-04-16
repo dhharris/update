f2077551d40b0f80f699ee8993c291bff1d97072
Hugh Harris <dhharris9@gmail.com>
2019-04-15 19:30:32 -0700

    General improvements for fetch_prices.py

--- fetch_prices.py
+++ fetch_prices.py
@@ -3,41 +3,61 @@ import os
 
 from urllib.request import urlopen
 
-# The notifier function
-def notify(title, subtitle, message):
+FILEPATH = "/tmp/runescape.log"
+
+
+class Item:
+    def __init__(self, name, desc, price, delta):
+        self.name = name
+        self.description = desc
+        self.price = price
+        self.delta = delta
+
+    @classmethod
+    def from_json_data(cls, data):
+        item = data["item"]
+        return cls(
+            item["name"],
+            item["description"],
+            item["current"]["price"],
+            item["today"]["price"],
+        )
+
+    def __repr__(self):
+        return "\n".join([
+            self.name,
+            self.description,
+            f"Current price: {self.price}",
+            f"Change: {self.delta}",
+        ])
+
+
+def notify(title: str, subtitle: str, message: str):
     t = "-title {!r}".format(title)
     s = "-subtitle {!r}".format(subtitle)
     m = "-message {!r}".format(message)
     os.system("/usr/local/bin/terminal-notifier {}".format(" ".join([m, t, s])))
 
-def print_item_price(id):
+
+def get_item_by_id(item_id: int) -> Item:
     """
-    Given an item id, prints data about the item
-    and its price on the Grand Exchange
+    Given an item id, returns json formatted data about the item and its price
+    on the Grand Exchange
     """
-    base_url = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item="
-    url = base_url + str(id)
-    # Get json object from server
+    base_url = "http://services.runescape.com/m=itemdb_oldschool/api/" \
+        "catalogue/detail.json?item="
+    url = base_url + str(item_id)
+
     response = urlopen(url)
     data = json.loads(response.read())
+    return Item.from_json_data(data)
 
-    # print(header
-    print(data["item"]["name"])
-    print(data["item"]["description"])
-
-    # print the current price
-    print("Current price: " + str(data["item"]["current"]["price"]))
-
-    # print(the price change for today
-    print("Change: " + str(data["item"]["today"]["price"]))
-
-    print()
 
 # astral rune
-print_item_price(9075)
+item = get_item_by_id(9075)
+with open(FILEPATH, "a") as f:
+    f.write(str(item))
 
-# cannonball
-#print_item_price(2)
 
 notify("Grand Exchange", "Prices updated", "See /tmp/runescape.log for info")
 os.system("/usr/bin/open /tmp/runescape.log")
